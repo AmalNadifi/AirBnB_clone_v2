2cb7b22c0134160e4141adb8980a328250dc67e8
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-08 09:54:29 -0800

    modified the code to include adding other sub directories

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,18 @@
 # Installing Nginx if not already installed
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 # Creating necessary directories if they do not exist
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
+sudo touch /data/web_static/releases/test/index.html
 
 # Creating a fake HTML FILE
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -20,7 +25,7 @@ echo "<html>
 
 # Creating or recreating the symbolic link
 sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Giving the ownership to the ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
