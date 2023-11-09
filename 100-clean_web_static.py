017404efcaadf720207c276d38f4b2da96d40e86
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-09 08:06:31 -0800

    pep8 fixed

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
 The following script defines a Fabric task to clean
-out-of-date archives on web servers.
+out-of-date archives on web servers
 """
-
 import os
 from fabric.api import *
 
@@ -13,6 +12,7 @@ web_server_2_IP = '52.206.72.6'
 
 # Setting the Fabric environment to the target IPs
 env.hosts = [web_server_1_IP, web_server_2_IP]
+
 
 def do_clean(number=0):
     """
@@ -28,16 +28,15 @@ def do_clean(number=0):
         None
     """
     number = 1 if int(number) == 0 else int(number)
-
     # Deleting local out-of-date archives
     local_archives = sorted(os.listdir("versions"))
     [local_archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(archive)) for archive in local_archives]
+        [local("rm ./{}".format(arch)) for arch in local_archives]
 
     # Deleting remote out-of-date archives
     with cd("/data/web_static/releases"):
-        remote_archives = run("ls -tr").split()
-        remote_archives = [archive for archive in remote_archives if "web_static_" in archive]
-        [remote_archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(archive)) for archive in remote_archives]
+        r_archives = run("ls -tr").split()
+        r_archives = [arch for arch in r_archives if "web_static_" in arch]
+        [r_archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(arch) for arch in r_archives]
