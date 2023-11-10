2a69901fea10e1b5799b6508565af21dbf0ccee5
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-10 06:33:10 -0800

    Puppet for setup modified

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,101 +1,111 @@
-# Configuring a web server for deployment of web_static.
+# 101-setup_web_static.pp
 
-# Defining the Nginx configuration file
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-# Ensuring that the 'nginx' package is present and installed
+# Ensuring Nginx is installed
 package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
-
-# Ensuring the existence of required directories and files
-file { '/data':
-  ensure  => 'directory'
-} ->
-
-file { '/data/web_static':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
-# Creating an 'index.html' file with content for the test release
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
-
-# Creating a symbolic link to the 'test' release
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
-
-# Changing ownership of the '/data/' directory to 'ubuntu'
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  ensure => 'installed',
 }
 
-# Ensuring the existence of required directories and files in '/var/www/' and '/var/www/html/'
-file { '/var/www':
-  ensure => 'directory'
+# Ensuring the existence of required directories
+file { '/data':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
 } ->
-
-file { '/var/www/html':
-  ensure => 'directory'
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
 } ->
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+} ->
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+} ->
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
 
-# Creating an 'index.html' file with content for the default HTML page
-file { '/var/www/html/index.html':
+# Creating an 'index.html' file for the test release
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
-} ->
+  content => @(HTML)
+    <html>
+      <head>
+      </head>
+      <body>
+        Holberton School
+      </body>
+    </html>
+  HTML
+}
 
-# Creating a custom '404.html' page
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+# Setting ownership for the 'index.html' file
+exec { 'set_owner_index_html':
+  command => 'chown ubuntu:ubuntu /data/web_static/releases/test/index.html',
+  path    => '/bin:/usr/bin',
+  require => File['/data/web_static/releases/test/index.html'],
+}
 
-# Updating the Nginx default configuration with the custom configuration
+# Setting mode for the 'index.html' file
+exec { 'set_mode_index_html':
+  command => 'chmod 0644 /data/web_static/releases/test/index.html',
+  path    => '/bin:/usr/bin',
+  require => File['/data/web_static/releases/test/index.html'],
+}
+
+# Creating or recreating the symbolic link
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  force  => true,
+}
+
+# Updating Nginx configuration
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf
+  content => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By ${hostname};
+      root   /var/www/html;
+      index  index.html index.htm;
+
+      location /hbnb_static {
+          alias /data/web_static/current;
+          index index.html index.htm;
+      }
+
+      location /redirect_me {
+          return 301 http://github.com/AmalNadifi;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ",
 } ->
 
-# Restarting the Nginx service
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+# Restarting Nginx service
+exec { 'nginx_restart':
+  command => '/bin/systemctl restart nginx',
+  path    => '/bin:/usr/bin',
+  require => File['/etc/nginx/sites-available/default'],
 }
