864b6d0e4107420c55892637eaf379b533db32df
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-25 09:29:12 -0800

    pep8 fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,16 @@ from flask import Flask
 app = Flask(__name__)
 
 # Defining a route for ("/") & disable strict slashes for flexibility
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """This function will be executed when the root URL is accessed """
     return "Hello HBNB!"
 
 # Running the Flask application if this script is executed directly
+
+
 if __name__ == "__main__":
     # Starting the Flask development server to listen on 0.0.0.0:5000
     app.run(host="0.0.0.0", port=5000)
