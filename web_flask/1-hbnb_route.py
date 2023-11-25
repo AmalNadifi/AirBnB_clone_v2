2be1d6c3d43c0bc8ab69457aebf22f36dda58b49
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-25 09:30:47 -0800

    pep8 fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,6 +9,8 @@ from flask import Flask
 app = Flask("__name__")
 
 # Defining a route for the ("/") and disable strict slashes for flexibility
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
@@ -16,12 +18,16 @@ def hello():
     return ("Hello HBNB!")
 
 # Defining a route for "/hbnb" and disabling strict slashes
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """This function will be executed when the "/hbnb" URL is accessed"""
     return ("HBNB")
 
 # Running the Flask application if this script is executed directly
+
+
 if __name__ == "__main__":
     # Starting the Flask development server to listen on 0.0.0.0:5000
     app.run(host="0.0.0.0", port=5000, debug=None)
